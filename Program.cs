b28698e4b6009ee0dbf1a010771f79a5047fe9bc
FabioTrindade <fabio.trindade@hotmail.com>
2023-01-22 00:53:38 +0000

    Adjustment in program port  to deploy Railway 1.1

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.UseUrls("http://*:" + Environment.GetEnvironmentVariable("PORT"));
+
 // Add services to the container.
 builder.Services.AddRazorPages();
 
